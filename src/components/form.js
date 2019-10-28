077e55ea91bd84e5180b6950d90d8f9eefc549db
Glyn L <glynlewington@gmail.com>
2019-10-28 19:45:55 +1000

    fixed form

--- src/components/form.js
+++ src/components/form.js
@@ -23,6 +23,21 @@ const StyledInput = styled.input`
 
 const StyledTextarea = styled(StyledInput)``
 
+const StyledButton = styled.button`
+  justify-self: flex-end;
+
+  border: 1px solid #333;
+  box-shadow: var(--box-shadow);
+  border-radius: 3px;
+  background: #fdfdfd;
+  color: #333;
+  padding: 5px 15px;
+
+  :active {
+    box-shadow: none;
+  }
+`
+
 const Form = ({ address, center }) => {
   return (
     <StyledForm
@@ -36,9 +51,10 @@ const Form = ({ address, center }) => {
         type="email"
         name="_replyto"
         placeholder="example@email.com"
+        required
       />
       <StyledLabel htmlFor="name">Name:</StyledLabel>
-      <StyledInput id="name" type="text" placeholder="John Doe" />
+      <StyledInput id="name" type="text" placeholder="John Doe" required />
       <StyledLabel htmlFor="text">Message:</StyledLabel>
       <StyledInput
         as="textarea"
@@ -47,9 +63,13 @@ const Form = ({ address, center }) => {
         rows="10"
         cols="20"
         placeholder="I'd love to know more about your events and membership."
+        required
       />
       {/* honeypot spam bot field */}
       <input type="text" name="_gotcha" style={{ display: 'none' }} />
+      <input type="hidden" name="_next" value="/" />
+      <span></span>
+      <StyledButton>Send</StyledButton>
     </StyledForm>
   )
 }

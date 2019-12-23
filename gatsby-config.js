a9c2cd7d86d8e365d498ea663909442bde2679f6
Glyn L <glynlewington@gmail.com>
2019-12-23 10:12:31 +1000

    metadata updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'AS Australia',
     description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+      'AS Australia - Organisation for those suffering from Ankylosing Spondylitis in Australia.',
+    author: 'AS Australia',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
